768b7dcf73753f00e6322e6984a48c798a5e45b0
agusamblard <aamblard@udesa.edu.ar>
2025-05-08 11:07:34 -0300

    changes to directory.c and pathname.c

--- TP3-FileSystem/directory.c
+++ TP3-FileSystem/directory.c
@@ -2,15 +2,42 @@
 #include "inode.h"
 #include "diskimg.h"
 #include "file.h"
+#include "direntv6.h"
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
 
-/**
- * TODO
- */
+#define DIRNAMELEN 14  // Definimos explícitamente el largo del nombre
+
 int directory_findname(struct unixfilesystem *fs, const char *name,
-		int dirinumber, struct direntv6 *dirEnt) {
-  //Implement your code here
-  return 0;
+                       int dirinumber, struct direntv6 *dirEnt) {
+    int blockNum = 0;
+    char block[DISKIMG_SECTOR_SIZE];
+
+    while (1) {
+        int nbytes = file_getblock(fs, dirinumber, blockNum, block);
+        if (nbytes == -1) return -1;
+        if (nbytes == 0) break; // fin del archivo
+
+        int nentries = nbytes / sizeof(struct direntv6);
+        struct direntv6 *entry = (struct direntv6 *) block;
+
+        for (int i = 0; i < nentries; i++) {
+            if (entry[i].d_inumber == 0) continue;
+
+            // Copiamos y aseguramos terminación nula del nombre
+            char filename[DIRNAMELEN + 1];
+            strncpy(filename, entry[i].d_name, DIRNAMELEN);
+            filename[DIRNAMELEN] = '\0';
+
+            if (strncmp(name, filename, DIRNAMELEN) == 0) {
+                *dirEnt = entry[i];
+                return 0;
+            }
+        }
+
+        blockNum++;
+    }
+
+    return -1; // no se encontró el nombre
 }
--- TP3-FileSystem/pathname.c
+++ TP3-FileSystem/pathname.c
@@ -7,10 +7,31 @@
 #include <string.h>
 #include <assert.h>
 
-/**
- * TODO
- */
 int pathname_lookup(struct unixfilesystem *fs, const char *pathname) {
-    //Implement code here
-	return 0;
+    if (strcmp(pathname, "/") == 0) {
+        return 1; // raíz
+    }
+
+    if (pathname[0] != '/') {
+        return -1; // solo rutas absolutas
+    }
+
+    char path_copy[strlen(pathname) + 1];
+    strcpy(path_copy, pathname);
+
+    char *token = strtok(path_copy, "/");
+    int current_inumber = 1; // raíz
+
+    struct direntv6 entry;
+
+    while (token != NULL) {
+        if (directory_findname(fs, token, current_inumber, &entry) < 0) {
+            return -1;
+        }
+
+        current_inumber = entry.d_inumber;
+        token = strtok(NULL, "/");
+    }
+
+    return current_inumber;
 }
