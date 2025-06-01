39ceabedb10739bdcb3403fbc8932809461ec671
agusamblard <aamblard@udesa.edu.ar>
2025-06-01 16:54:56 -0300

    manejo correcto no numericos

--- TP4-Shell/src/ej1/ring.c
+++ TP4-Shell/src/ej1/ring.c
@@ -17,31 +17,42 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
-    n = atoi(argv[1]);
-	char *endptr;
-	buffer[0] = strtol(argv[2], &endptr, 10);
-	if (*endptr != '\\0') {
-		fprintf(stderr, "Error: el valor inicial '%s' no es un número válido.\\n", argv[2]);
-		exit(1);
-	}
-    start = atoi(argv[3]);
+    char *endptr_n, *endptr_c, *endptr_s;
+
+    n = strtol(argv[1], &endptr_n, 10);
+    if (*endptr_n != '\0') {
+        fprintf(stderr, "Error: el valor de <n> ('%s') no es un número válido.\n", argv[1]);
+        exit(1);
+    }
+
+    buffer[0] = strtol(argv[2], &endptr_c, 10);
+    if (*endptr_c != '\0') {
+        fprintf(stderr, "Error: el valor de <c> ('%s') no es un número válido.\n", argv[2]);
+        exit(1);
+    }
+
+    start = strtol(argv[3], &endptr_s, 10);
+    if (*endptr_s != '\0') {
+        fprintf(stderr, "Error: el valor de <s> ('%s') no es un número válido.\n", argv[3]);
+        exit(1);
+    }
 
     if (n < 3) {
         fprintf(stderr, "Error: La cantidad de procesos (n = %d) debe ser mayor o igual a 3.\n", n);
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 
     if (start < 0) {
         fprintf(stderr, "Error: El índice del nodo de inicio (s = %d) no puede ser negativo.\n", start);
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 
     if (start >= n) {
         fprintf(stderr, "Error: El índice del nodo de inicio (s = %d) debe ser menor que n (n = %d).\n", start, n);
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 
-
+    printf("Se crearán %i procesos, se enviará el mensaje %i desde proceso %i\n", n, buffer[0], start);
 
     int pipes[n][2];
     for (int i = 0; i < n; i++) {
@@ -63,14 +74,14 @@ int main(int argc, char **argv) {
             close(pipes[i][0]);    // Cierro lectura del mío
 
             if (i == start) {
-
+                printf("Proceso %d (PID %d) recibió el mensaje: %d\n", i, getpid(), buffer[0]);
                 buffer[0]++;
                 write(pipes[i][1], buffer, sizeof(int));
                 read(pipes[prev][0], buffer, sizeof(int));
                 printf("Proceso %d (PID %d) recibió el mensaje final: %d\n", i, getpid(), buffer[0]);
             } else {
                 read(pipes[prev][0], buffer, sizeof(int));
-
+                printf("Proceso %d (PID %d) recibió el mensaje: %d\n", i, getpid(), buffer[0]);
                 buffer[0]++;
                 write(pipes[i][1], buffer, sizeof(int));
             }
